fa3daac154e4301ebe23838b1751ef4f31cc4d74
ijapesigan <ijapesigan@gmail.com>
2025-01-04 06:26:54 +0000

    Automated build.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+bash pandoc.sh
 bash mplus.sh
 bash ubuntu_cran_pkg.sh
 bash rproject.sh
--- scripts/ubuntu_cran_pkg.sh
+++ scripts/ubuntu_cran_pkg.sh
@@ -5,3 +5,11 @@ set -e
 # mplus automation
 install2.r --error --skipinstalled -n -1 \
     MplusAutomation
+
+# etc
+install2.r --error --skipinstalled -n -1 \
+    mvtnorm                              \
+    matrixStats                          \
+    sp
+
+Rscript -e "install.packages(\"BiocManager\"); BiocManager::install(\"rhdf5\")"
