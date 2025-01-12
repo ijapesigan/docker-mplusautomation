1f1cf080fdc51e822281459b0f224fa25b1e5c75
ijapesigan <ijapesigan@gmail.com>
2025-01-12 22:51:56 +0000

    Automated build.

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+cp ~/work/ComboLinux64.bin scripts
+
 docker build -t mplusautomation:latest .
 apptainer build mplusautomation.sif docker-daemon://mplusautomation:latest
--- scripts/mplus.sh
+++ scripts/mplus.sh
@@ -2,7 +2,5 @@
 
 set -e
 
-# Copy ComboLinux64.bin to scripts/
-
 ./ComboLinux64.bin -i silent
 ln -s /opt/mplus/8.11/mplus /usr/local/bin/mplus
