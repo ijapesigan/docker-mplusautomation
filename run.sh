cdbe2a3a8d49828ab32056c99da7bdbaafb3e794
jeksterslab <learn.jeksterslab@gmail.com>
2026-04-09 05:52:40 +0000

    Automated build.

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ cp ~/work/ComboLinux64.bin scripts
 
 docker build -t mplusautomation:latest .
 apptainer build mplusautomation.sif docker-daemon://mplusautomation:latest
+cp mplusautomation.sif ~/work/
