e7fb2f680045c87e47d6e4e6bd72c7f1bdd21b62
jeksterslab <learn.jeksterslab@gmail.com>
2026-05-20 21:27:05 +0000

    Automated build.

--- run.sh
+++ run.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-cp /storage/home/ibp5092/work/software/mplus-9.01/ComboLinux64.bin scripts
+mkdir -p "/storage/home/${USER}/work/apptainer/tmp"
+mkdir -p "/storage/home/${USER}/work/apptainer/cache"
+mkdir -p "/storage/home/${USER}/work/sif"
+
+chmod 700 "/storage/home/${USER}/work/apptainer/tmp"
+chmod 700 "/storage/home/${USER}/work/apptainer/cache"
+
+export APPTAINER_TMPDIR="/storage/home/${USER}/work/apptainer/tmp"
+export APPTAINER_CACHEDIR="/storage/home/${USER}/work/apptainer/cache"
+
+cp /storage/home/${USER}/work/software/mplus-9.01/ComboLinux64.bin scripts
 
 docker build -t mplusautomation:latest .
 apptainer build mplusautomation.sif docker-daemon://mplusautomation:latest
-cp mplusautomation.sif /storage/home/ibp5092/work/sif
+cp mplusautomation.sif /storage/home/${USER}/work/sif
